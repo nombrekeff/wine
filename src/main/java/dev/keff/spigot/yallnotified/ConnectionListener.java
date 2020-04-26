45bd39b889eb05aa93b6563540469bcb23feb512
nombrekeff <manoloedge96@gmail.com>
2020-04-26 16:46:05 +0200

    feat: some improvements, improved custom messages and added DeathEvent

--- src/main/java/dev/keff/spigot/yallnotified/ConnectionListener.java
+++ src/main/java/dev/keff/spigot/yallnotified/ConnectionListener.java
@@ -2,61 +2,89 @@ package dev.keff.spigot.yallnotified;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.apache.commons.lang.text.StrSubstitutor;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ConnectionListener implements Listener {
 
+    static String EVENT_ON_JOIN = "onJoin";
+
     Notifier notifier;
     FileConfiguration config;
+    Logger logger;
 
     ConnectionListener(Notifier notifier, FileConfiguration config) {
         this.notifier = notifier;
         this.config = config;
+        this.logger = Bukkit.getLogger();
     }
 
-    public String formatMessage(String path, String playerName) {
+    public String formatMessage(String path, Map<String, String> values) {
         String template = config.getString(path);
-        Bukkit.getLogger().info("template " + config.getString(template));
-
-        Map<String, String> values = new HashMap<>();
-        values.put("username", playerName);
-
         String message = StrSubstitutor.replace(template, values, "{", "}");
-
         return message;
+    }
+
+    public void notifyPlayerEvent(PlayerEvent event) {
+        String eventName = event.getEventName();
+        this.logger.info("eventName: " + eventName);
+
+        if (config.getBoolean("telegram.events." + eventName)) {
+            String name = event.getPlayer().getName();
+            Map<String, String> values = new HashMap<>();
+            values.put("name", name);
+
+            String outputMsg = formatMessage("telegram.message_formats." + eventName, values);
+            notifier.notify(outputMsg);
+        }
+    }
+
+    public void notifyEntityEvent(EntityEvent event) {
+        String eventName = event.getEventName();
+        this.logger.info("eventName: " + eventName);
+
+        if (config.getBoolean("telegram.events." + eventName)) {
+            Entity entity = event.getEntity();
+            String name = entity.getName();
+            Location deathLocation = entity.getLocation();
+
+            Map<String, String> values = new HashMap<>();
+            values.put("name", name);
+            values.put("death_x", Double.toString(deathLocation.getX()));
+            values.put("death_y", Double.toString(deathLocation.getY()));
+            values.put("death_z", Double.toString(deathLocation.getZ()));
+            values.put("death_cause", event.getEntity().getLastDamageCause().toString());
+
+            String outputMsg = formatMessage("telegram.message_formats." + eventName, values);
+            notifier.notify(outputMsg);
+        }
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        if (config.getBoolean("telegram.events.onJoin")) {
-            String playerName = event.getPlayer().getName();
-            String chatMsg = "Hey there, " + playerName + ", welcome!";
-            String outputMsg = formatMessage("telegram.message_formats.onJoin", playerName);
-
-            Bukkit.getLogger().info(chatMsg);
-            event.getPlayer().sendMessage(chatMsg);
-            notifier.notify(outputMsg);
-        }
-
+        this.notifyPlayerEvent(event);
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        if (config.getBoolean("telegram.events.onQuit")) {
-            String playerName = event.getPlayer().getName();
-            String chatMsg = "Bye bye, " + playerName;
-            String outputMsg = formatMessage("telegram.message_formats.onQuit", playerName);
-
-            Bukkit.getLogger().info(chatMsg);
-            event.getPlayer().sendMessage(chatMsg);
-            notifier.notify(outputMsg);
-        }
+        this.notifyPlayerEvent(event);
     }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        this.notifyEntityEvent(event);
+    }
+
 }
